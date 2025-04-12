9556b8695bf8f20584c9ea2218cf24d5f18af2f4
CPL <carlos@centropyme.com>
2025-04-12 19:42:36 +0200

    Paso 6 * Creamos las otras consultas que va a utilizar nuestra aplicación:
    a) Si incluimos cada resulset con la consulta dentro de la lista de recursos del try, es obligatorio usar una sentencia distinta para cada resulset, ya que de usar la misma, el segundo resulset cerraría la consulta antes de ejecutarse, lo que genera un error).

--- src/main/java/PatronSingleton.java
+++ src/main/java/PatronSingleton.java
@@ -6,13 +6,23 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public static void main(String[] args) {
-    // Ahora utilizamos el singleton que hemos creado en ConexionBD
+
+    // Ahora utilizamos el singleton que hemos creado en ConexionBD.
+    // Como vamos a ejecutar varias sentencias SQL, hay que incluirlos en el try con recursos
     try (Connection conex = ConexionBD.creaConexion();
          Statement stmt = conex.createStatement();
-         ResultSet rs = stmt.executeQuery("SELECT * FROM cliente")){
+         ResultSet rs = stmt.executeQuery("SELECT * FROM cliente");
+         Statement stmt2 = conex.createStatement();
+         ResultSet rs2 = stmt2.executeQuery("SELECT * FROM empleado")
+         ){
 
+        System.out.println("--------------- Lista de clientes --------------------");
         while(rs.next()){
             System.out.println(rs.getInt("codigo_cliente") + " - " + rs.getString("nombre_cliente"));
+        }
+        System.out.println("--------------- Lista de empleados --------------------");
+        while(rs2.next()){
+            System.out.println(rs2.getInt("codigo_empleado") + " - " + rs2.getString("nombre"));
         }
     } catch (SQLException e) {
         throw new RuntimeException(e);
