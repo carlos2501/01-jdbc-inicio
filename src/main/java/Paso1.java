7b379d626b86e3bb36cd99a90151e1b7bd0b787b
CPL <carlos@centropyme.com>
2025-04-12 12:30:34 +0200

    Paso 3 * Utilizamos "finally" para asegurar que la conexión se cierra aunque falle la consulta. * Esta forma de proceder da lugar a una situación recursiva (la misma operación de cierre también puede fallar, por lo que deberíamos repetir el procese de protección para asegurar el cierre)

--- src/main/java/Paso1.java
+++ src/main/java/Paso1.java
@@ -6,22 +6,35 @@ public static void main(String[] args) {
     String usu = "root";
     String clave = "carlospdl%";
 
+    // Es necesario inicializar las variables que se van a utilizar en el bloque try-catch
+    Connection conex = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+
     // Bloque de conexión y procesamiento de la respuesta.
     // Recordar que la respuesta es SIEMPRE un conjunto de registros (aunque sea un conjunto vacío -null-)
     try {
-        Connection conex = DriverManager.getConnection(url, usu, clave);
-        Statement stmt = conex.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM cliente");
+        conex = DriverManager.getConnection(url, usu, clave);
+        stmt = conex.createStatement();
+        rs = stmt.executeQuery("SELECT * FROM cliente");
 
         // Si existen datos...
         while (rs.next()) {
             System.out.println(rs.getInt("codigo_cliente") + " - " + rs.getString("nombre_cliente"));
         }
-        // cerramos la conexión
-        rs.close();
-        stmt.close();
-        conex.close();
+
     } catch (SQLException e) {
         throw new RuntimeException(e);
+    } finally {
+        /** Al utilizar el finally, nos aaseguramos de que SIEMPRE se cierra la coneción. Sin embargo, como puede
+         *  fallar la comunicación es necesario usar el bloque try-catch
+         */
+        try {
+            rs.close();
+            stmt.close();
+            conex.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
